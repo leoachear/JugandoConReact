b2d63987bfb25f87133759417d74866c345754ad
@leoachear <leoachear@gmail.com>
2021-04-28 18:55:59 -0300

    Props

--- src/index.js
+++ src/index.js
@@ -8,9 +8,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hello, Platzi Badges!</h1>;
+import "bootstrap/dist/css/bootstrap.css";
+
+import "./global.css";
+import Badge from './components/Badge';
+
+// const element = (
+//     <div>
+//         <h1>Hola, soy Leo</h1>
+//         <p>Soy futuro Ingeniero Forntend</p>
+//     </div>
+// );
 
 const container = document.getElementById('app');
 
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(element, container);
+ReactDOM.render(
+    <Badge 
+        firstName="Leo" 
+        lastName="Achear" 
+        jobTitle="Frontend Engineer" 
+        twitter="leoachear"
+        avatarUrl="https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon"
+    />,
+    container
+);
+
